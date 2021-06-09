4b7beb25809d61b776dcbeac3ff1cc21d76ee60f
aCATdemy <73630343+aCATdemy@users.noreply.github.com>
2021-06-09 15:55:10 +0800

    Updated aCATServer Portal

--- en-sg/acatserver-portal/scripts/navbar.js
+++ en-sg/acatserver-portal/scripts/navbar.js
@@ -1,18 +1,18 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
-var navbarcol = document.getElementById("navbarcol");
+var navbarcolour = document.getElementById("navbarcol");
 
 var sticky = navbar.offsetTop;
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
-        navbarcol.classList.remove("bg-light");
-        navbarcol.classList.add("bg-warning");
+        navbarcolour.classList.remove("bg-light");
+        navbarcolour.classList.add("bg-warning");
     } else {
         navbar.classList.remove("sticky");
-        navbarcol.classList.remove("bg-warning");
-        navbarcol.classList.add("bg-light");
+        navbarcolour.classList.remove("bg-warning");
+        navbarcolour.classList.add("bg-light");
     }
 }
